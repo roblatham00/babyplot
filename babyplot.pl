c7cd0adf59ebdfa74ca54d70858e32e09144d289
Rob Latham <robl@mcs.anl.gov>
2012-12-13 10:19:36 -0600

    datemath
    figured out how that works

--- babyplot.pl
+++ babyplot.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl 
 
 use warnings;
+#use strict;
 
 use SVG;
 use Date::Parse;
@@ -14,7 +15,8 @@ my $daystart="17:00";
 # - can put x and y coordinates and labels on outter coordinate system
 # - can put events on an inner translated system
 
-my %days;
+my @events = {};
+# events will have N parts (day, start, stop, color)
 
 # we are processing logs after the fact, so entries should all be before this one
 my $firstday = DateTime->now( time_zone => 'local' )->set_time_zone('floating');
@@ -22,7 +24,6 @@ my $lastday = DateTime->from_epoch(epoch=> 0);
 
 while (<>) {
 	($start,$end,$duration,$activity,$quantity,$unit,$desc,$notes,$annoter) = split(/,/);
-	#print "$activity\n";
 	# so, notinally pretty simple: just draw a box for each event.  Sleep
 	# for one hour? draw an hour-long box.  
 	# - but, discard records we don't care about
@@ -32,10 +33,8 @@ while (<>) {
 	#
 	# - since i don't care about the order the boxes are printed, use a
 	# hash whose keys are days and whose values are arrays of entries
-	#print "$start\n";
-	#($ss,$mm,$hh,$day,$month,$year,$zone) = strptime($start) or next;
-	# the year is getting messed up.  i guess strptime needs a little help.
-	# not hard to go back to doing it by hand
+
+	# strptime did not like way date was formatted
 	@time = split(/[:\/ ]/, $start);
 	next if (@time != 5);
 	($month,$day,$year,$hh,$mm) = @time;
@@ -53,6 +52,12 @@ while (<>) {
 	if ((DateTime->compare($date, $lastday)) == 1) {
 		$lastday = $date;
 	}
+
+	# since i eventually want to start the plot for a day at 5pm, i may
+	# want to look not just at days but at hours and minutes too: 
+	# my $ndays = $date->subtract_datetime_absolute($firstday)->delta_seconds / (24*60*60);
+	my $ndays = $date->delta_days($firstday)->delta_days;;
+	print "$ndays\n";
 
 	if ($activity eq '"Sleep"') {
 		# easier to read if days start at 5pm.
