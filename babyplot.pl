8ac781872df3d1814be34d3689087a26f5ee498f
Rob Latham <robl@mcs.anl.gov>
2013-01-16 22:22:25 -0600

    makes a picture!
    generates the svg image now.  Still missing: labels so hu-mans can
    actually understand what it's trying to show.

--- babyplot.pl
+++ babyplot.pl
@@ -22,6 +22,7 @@ my @events = {};
 # we are processing logs after the fact, so entries should all be before this one
 my $firstday = DateTime->now( time_zone => 'local' )->set_time_zone('floating');
 my $lastday = DateTime->from_epoch(epoch=> 0);
+my $ndays = 1;
 
 while (<>) {
 	($start,$end,$duration,$activity,$quantity,$unit,$desc,$notes,$annoter) = split(/,/);
@@ -91,13 +92,13 @@ while (<>) {
 		time_zone => 'floating');
 
 	my $start_in_seconds = $begin->subtract_datetime_absolute($firstday)->delta_seconds;
-	my $ndays = int(($start_in_seconds) / (24*60*60));
+	$ndays = int(($start_in_seconds) / (24*60*60));
 	my $minutes = ($start_in_seconds - ($ndays*24*60*60))/60;
 
 	#my $ndays = $begin->delta_days($firstday)->delta_days;
 
 	if ($activity eq '"Sleep"') {
-		print "$_";
+		# the {} notation creates not a hash, but a reference to a hash
 		push @events, { "start" => $minutes, 
 			"duration" => $duration,
 			"day" => $ndays,
@@ -109,11 +110,31 @@ while (<>) {
 shift @events;
 
 my $count = @events;
-print "found $count events\n";
+#print "found $count events\n";
 
+my $svg = SVG->new();
+
+$ndays = $ndays + 1;
 foreach $item (@events)
 {
 	# $item is a *hash reference*
-	print "$item  ";
-	print "Sleep at $item->{'start'} for $item->{'duration'} on day $item->{'day'}\n";
+	#print "Sleep at $item->{'start'} for $item->{'duration'} on day $item->{'day'}\n";
+	my $x = ($item->{'start'}/1440)*100;
+	my $x_str = "$x" . "%";
+
+	my $y = ($item->{'day'}/$ndays) * 100;
+	my $y_str = "$y" . "%";
+
+	my $width = ($item->{'duration'}/1440) * 100;
+	my $width_str = "$width" . "%";
+
+	my $height = (1/$ndays)*100;
+	my $height_str = "$height" . "%";
+
+	#print "x: $x_str y: $y_str width: $width_str height $height_str\n";
+	$svg->rectangle(x=>$x_str, y=>$y_str, 
+		height=>$height_str, width=>$width_str,
+	        style=>{'fill'=>'rgb(0,255,0)'});
 }
+
+print $svg->xmlify;
